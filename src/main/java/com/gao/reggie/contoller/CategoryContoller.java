a1ecfc2f4f3c8fcabbe3b65de21e1e4702296b0e
gaoqinshan <1626733251@qq.com>
2023-07-10 19:21:42 +0800

    分类管理

--- src/main/java/com/gao/reggie/contoller/CategoryContoller.java
+++ src/main/java/com/gao/reggie/contoller/CategoryContoller.java
@@ -1,8 +1,12 @@
 package com.gao.reggie.contoller;
 
+import com.gao.reggie.common.R;
+import com.gao.reggie.entity.Category;
 import com.gao.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,18 @@ public class CategoryContoller {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     *
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> save(@RequestBody Category category) {
+
+        log.info("category: {} ", category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 }
